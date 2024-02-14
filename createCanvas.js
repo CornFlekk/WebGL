d2be40a6f30588b1783c1117e5db9c271cf35e79
CornFlaek <luca.spary04@gmail.com>
2024-02-14 15:24:47 +0100

    deh peffo

--- createCanvas.js
+++
@@ -1,9 +0,0 @@
-const canvas = document.getElementById("glcanvas");
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-window.addEventListener("resize", () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-});
