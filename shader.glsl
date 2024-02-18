f3c2793d7850da5bebf88b32d30a43e92eb9bc04
CornFlaek <57416992+CornFlekk@users.noreply.github.com>
2024-02-18 10:42:05 +0100

    ??

--- shader.glsl
+++
@@ -1,9 +0,0 @@
-precision mediump float;
-
-uniform float uTime;
-uniform float uResolution;
-
-void main() {
-    vec2 st = gl_FragCoord.xy/uResolution;
-    gl_FragColor = vec4(sin(st.x*uTime),sin(st.y*uTime),st.y,1.0);
-}
